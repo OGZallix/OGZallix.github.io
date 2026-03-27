92a9ad3e2556f6f43f803269959fa50d4536e3af
OGZallix <55059970+OGZallix@users.noreply.github.com>
2026-03-27 18:07:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -638,7 +638,7 @@ loadBtn.onclick = () => {
 // === Fixed texture palette (non-editable) ===
 const fixedTextureFolders = [
     { name: 'Gilded', path: 'Textures/Roads/Gilded', files: [
-        'T1-Understated-1.png'.'T1-Understated-2.png'] },
+        'T1-Understated-1.png','T1-Understated-2.png'] },
     { name: 'Brick', path: 'Textures/Roads/Brick', files: [
         'T1-Brick-1.png','T1-Brick-2.png','T1-Brick-3.png','T1-Decorative-Brick-1.png','T2-Brick-1.png','T3-Brick-1.png','T3-Brick-2.png','T3-Decorative-Brick-1.png','T3-Decorative-Brick-2.png'] },
     { name: 'Stone', path: 'Textures/Roads/Stone', files: [
